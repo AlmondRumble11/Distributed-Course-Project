ded3cc995969bd78a90d19b5407fe25b2c5dd02b
AlmondRumble11 <jess.mustonen@gmail.com>
2022-04-12 19:07:39 +0300

    without threading

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
+from operator import index
+from os import link
+import queue
 from xmlrpc.client import ServerProxy
 import sys
 import time
@@ -5,6 +8,103 @@ import time
 #https://docs.python.org/3/library/xmlrpc.server.html
 #server proxy
 proxy = ServerProxy('http://localhost:1234')
+q = queue.Queue()
+
+
+class TreeNode:
+
+    def __init__(self, node):
+        self.node_value = node
+        self.parent = None
+        self.links = []
+
+
+    
+    def add_link(self, new_link, parent):
+        
+        new_child = TreeNode(new_link)
+        new_child.parent = parent
+        parent.links.append(new_child)
+      
+        q.put(new_child)
+    
+def seach_tree(start_node, end_value):
+
+    if start_node is None:
+        return 
+    
+    
+    for link in start_node.links:
+
+        if (link.node_value == end_value):
+            print('Found the end value')
+            print(link.node_value, end_value)
+            return link
+
+    return start_node
+
+def print_path(head):
+    path =  []
+    temp = head
+    while temp is not None:
+        print(temp.node_value)
+        path.append(temp.node_value)
+        temp = temp.parent
+    
+    path.reverse()
+
+    for index in range(len(path)):
+                    
+        if(index +1 < len(path)):
+            print(path[index], end=' --> ')
+        else:
+            print(path[index])
+    return 
+
+def search (start, end):
+
+    #links = proxy.wikipediaSearch(start)
+    #print(links)
+
+    root_node = TreeNode(start)
+    value_found = False
+    search_term = start
+    success = False
+    own_queue = []
+    own_queue.append(start)
+    q.put(root_node)
+    while not q.empty():
+
+        q.task_done()
+        t1 = time.time()
+        links = proxy.wikipediaSearch(search_term)
+        t2 = time.time()
+        full = t2-t1
+        print(full)
+        for link in links:
+            root_node.add_link(link, root_node)
+        
+        value_found = seach_tree(root_node, end)
+        
+        if(value_found != root_node):
+            print_path(value_found)
+            success = True
+            break
+        else:
+           root_node = q.get()
+           search_term = root_node.node_value
+           print(q.qsize())
+           print(search_term)
+           success = False
+
+
+        
+
+
+    
+
+
+
 
 def Menu():
     print("**********************MENU****************************")
@@ -100,5 +200,13 @@ def main():
        
         else:
             print("No functions for that number\n") 
-main()
+#main()
+
+def Main():
+    st = time.time()
+    search("Halo 3", "New York Police Department")
+    et = time.time()
+    took = et-st
+    print(took)
+Main()
 
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 
 
+from collections import defaultdict
 import time
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 from xmlrpc.server import SimpleXMLRPCServer
+from matplotlib.pyplot import title
 import requests
 import threading, queue
 from multiprocessing.pool import ThreadPool
@@ -20,6 +22,25 @@ server = SimpleXMLRPCServer((host,port), requestHandler=RequestHandler,logReques
 q = queue.Queue()
 
 
+
+
+class TreeNode:
+
+    def __init__(self, node):
+        self.node_value = node
+        self.parent = None
+        self.links = []
+
+
+    
+    def add_link(self, new_link, parent):
+        
+        new_child = TreeNode(new_link)
+        new_child.parent = parent
+        parent.links.append(new_child)
+      
+        q.put(new_child)
+    
 
 def wikipediaSearch(start_topic):
 
@@ -44,7 +65,7 @@ def wikipediaSearch(start_topic):
     response = session.get(url=url, params=params)
     data = response.json()
     pages = data["query"]["pages"]
-
+    
     link_list = []
     
     try:
@@ -52,10 +73,108 @@ def wikipediaSearch(start_topic):
             for link in value["links"]:
                 if(link['ns'] == 0):
                     link_list.append(link["title"])
-                #print(link["title"])
+          
     except KeyError:
         pass
+
+    #https://stackoverflow.com/questions/14882571/how-to-get-all-urls-in-a-wikipedia-page
+    while True:
+        try:
+            extra_data = data['continue']['plcontinue']
+            params['plcontinue'] = extra_data
+
+            response = session.get(url=url, params=params)
+            data = response.json()
+            new_pages = data["query"]["pages"]
+            for key, value in new_pages.items():
+                for link in value["links"]:
+                    if(link['ns'] == 0):
+                        link_list.append(link["title"])
+        except KeyError:
+            break
+        
+    #print(len(link_list))
     return link_list
+
+def seach_tree(start_node, end_value):
+
+    if start_node is None:
+        return 
+    
+    
+    for link in start_node.links:
+
+        if (link.node_value == end_value):
+            print('Found the end value')
+            print(link.node_value, end_value)
+            return link
+
+    return start_node
+
+def print_path(head):
+    path =  []
+    temp = head
+    while temp is not None:
+        print(temp.node_value)
+        path.append(temp.node_value)
+        temp = temp.parent
+    
+    path.reverse()
+
+    for index in range(len(path)):
+                    
+        if(index +1 < len(path)):
+            print(path[index], end=' --> ')
+        else:
+            print(path[index])
+    return 
+
+def search (start, end):
+
+    #links = proxy.wikipediaSearch(start)
+    #print(links)
+
+    root_node = TreeNode(start)
+    value_found = False
+    search_term = start
+    success = False
+    own_queue = []
+    own_queue.append(start)
+    q.put(root_node)
+    while not q.empty():
+
+        q.task_done()
+        #t1 = time.time()
+        links = wikipediaSearch(search_term)
+       # t2 = time.time()
+        #full = t2-t1
+        #print(full)
+        for link in links:
+            root_node.add_link(link, root_node)
+        
+        value_found = seach_tree(root_node, end)
+        
+        if(value_found != root_node):
+            print_path(value_found)
+            success = True
+            break
+        else:
+           root_node = q.get()
+           search_term = root_node.node_value
+           #print(q.qsize())
+          # print(search_term)
+           success = False
+
+
+def Main2():
+    st = time.time()
+    search("Mämmi", "Fox")
+    et = time.time()
+    took = et-st
+    print(took)
+Main2()
+
+
 
 
 
@@ -112,14 +231,14 @@ def iterateList(graph,end_topic,visited,topic,link):
 
 def Main():
     begin_time = time.time()
-    start_topic = "Halo 3"
-    #start_topic = "Apple"
+    #start_topic = "Halo 3"
+    start_topic = "United States"
     #start_topic = "Elizabeth II"
     #end_topic = "Alligator"
     #end_topic = "Bread"
-    end_topic = "New York City Police Department"
+    #end_topic = "New York City Police Department"
     #end_topic = "New York City"
-    #end_topic = "London"
+    end_topic = "London"
     #end_topic = 'Elon Musk'
     #https://www.geeksforgeeks.org/shortest-path-unweighted-graph/
     graph = {}
@@ -131,41 +250,33 @@ def Main():
     visited.append(start_topic)
     index = 0
     topic_list = wikipediaSearch(start_topic) #0...280
+    
     while True:
 
         #get links for the topic name
-        #topic = visited[index]
-        #topic_list = wikipediaSearch(topic)
+        topic = visited[index]
+        topic_list = wikipediaSearch(topic)
         results = []
-        start_time = time.time()
-        pool = ThreadPool(30)
-        for i in topic_list:
-            results.append(pool.apply(wikipediaSearch, args=(i,)))
-        pool.close()
-        pool.join()
-        end_time = time.time()
-        print(end_time-start_time)
-        break
+      
+        
         #send_list = []
         #https://stackoverflow.com/questions/26104512/how-to-obtain-the-results-from-a-pool-of-threads-in-python/26104609#26104609
-        #pool = ThreadPool(30)
-        #results = []
-        #for link in topic_list:
- 
-          
-        #    results.append(pool.apply(iterateList, args=(graph,end_topic,visited,topic,link)))
-        #pool.close()
-        #pool.join()
+        pool = ThreadPool(30)
+        results = []
+        for link in topic_list:
+            results.append(pool.apply(iterateList, args=(graph,end_topic,visited,topic,link)))
+        pool.close()
+        pool.join()
         
-        #for r in results:
-        #    
+        for r in results:
+            
 
-        #    if(r[1] == True):
+            if(r[1] == True):
         #        print(r[0])
-        #        end_time = time.time()
-        #        taken_time = end_time - begin_time
-        #        print("It took",taken_time,"seconds")
-        #        return 0
+                end_time = time.time()
+                taken_time = end_time - begin_time
+                print("It took",taken_time,"seconds")
+                return 0
 
 
 #checking the correct seachterm
@@ -180,10 +291,10 @@ def checkCorrectParameters(parameter):
 
     #open search parameters
     params = {
-     "action": "query",
-    "format": "json",
-    "list": "search",
-    "srsearch": parameter
+        "action": "query",
+        "format": "json",
+        "list": "search",
+        "srsearch": parameter
     }
     
     #https://stackoverflow.com/questions/14882571/how-to-get-all-urls-in-a-wikipedia-page
@@ -204,12 +315,13 @@ def search(start, end):
     return ["Halo 3","7-Eleven","London"]
 server.register_function(checkCorrectParameters)
 server.register_function(search)
+server.register_function(wikipediaSearch)
 #Main()
 
-def main():
-    try:
-        print('Starting server')
-        server.serve_forever() #runnin the server
-    except KeyboardInterrupt:
-        print('Shutting down the server')
-main()
+#def main():
+#    try:
+#        print('Starting server')
+#        server.serve_forever() #runnin the server
+#    except KeyboardInterrupt:
+#        print('Shutting down the server')
+#main()
