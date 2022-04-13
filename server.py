cd550ad78fdf4fc0c9e88067818b2e741999f888
AlmondRumble11 <jess.mustonen@gmail.com>
2022-04-13 19:57:51 +0300

    commented

--- server.py
+++ server.py
@@ -1,52 +1,66 @@
 
 
-from collections import defaultdict
-from concurrent.futures import thread
 import json
 import time
-from xmlrpc.server import SimpleXMLRPCRequestHandler
 from xmlrpc.server import SimpleXMLRPCServer
-from matplotlib.pyplot import title
 import requests
 import threading, queue
-from multiprocessing.pool import ThreadPool
+from socketserver import ThreadingMixIn
+
+
 #https://www.youtube.com/watch?v=_8xXrFWcWao
 #https://docs.python.org/3/library/xmlrpc.server.html 
-#host and port
-host = 'localhost'
-port = 1234
+#https://stackoverflow.com/questions/53621682/multi-threaded-xml-rpc-python3-7-1
+#creates multihreaded server that can handle moany clients at the same time
+class SimpleThreadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
+    pass
+# run server
+def run_server():
 
-#server request handler
-class RequestHandler(SimpleXMLRPCRequestHandler):
-    rpc_paths = ('/RPC2',)
-#creating server
-server = SimpleXMLRPCServer((host,port), requestHandler=RequestHandler,logRequests=True ,allow_none=True,encoding='utf-8')
-q = queue.Queue()
-END = False
-Client_list =  []
+    #port, host and address
+    host = 'localhost'
+    port = 1234
+    server_addr = (host, port)
+
+    #create server and add functions that clients can use
+    server = SimpleThreadedXMLRPCServer(server_addr)
+    server.register_function(checkCorrectParameters)
+    server.register_function(search)
+    print("Server started...")
+    print('listening on {} port {}'.format(host, port))
+    server.serve_forever()
+
+
+#query class
+class Query:
+
+    def __init__(self):
+        self.END = False #found end point
+        self.path = [] #path to the end point
+        
 
 
 #https://stackoverflow.com/questions/2358045/how-can-i-implement-a-tree-in-python 
 class TreeNode:
 
+    #tree object
     def __init__(self, node):
         self.node_value = node
         self.parent = None
         self.links = []
 
 
-    
+    #add to list
     def add_link(self, new_link, parent):
         
+        #create new node and add it to parent and parent to it
         new_child = TreeNode(new_link)
         new_child.parent = parent
         parent.links.append(new_child)
       
-        q.put(new_child)
+        #q.put(new_child)
         
-
-    
-
+#wikipedia seach to ge tall of the links
 def wikipediaSearch(start_topic):
 
   
@@ -67,240 +81,194 @@ def wikipediaSearch(start_topic):
     }
     
     #https://stackoverflow.com/questions/14882571/how-to-get-all-urls-in-a-wikipedia-page
+    #response
     response = session.get(url=url, params=params)
     data = response.json()
+
+    #get page data
     pages = data["query"]["pages"]
     
+    #list of link for the topic
     link_list = []
     
     try:
+        #items from the page
         for key, value in pages.items():
+
+            #links from the page
             for link in value["links"]:
+
+                #is regular link
                 if(link['ns'] == 0):
                     link_list.append(link["title"])
-          
+
+    #no links found  
     except KeyError:
         pass
 
     #https://stackoverflow.com/questions/14882571/how-to-get-all-urls-in-a-wikipedia-page
+    #get all of the links as max number in one request is 500
     while True:
+
+        #keyerror --> no more links
         try:
+
+            #get extra pages
             extra_data = data['continue']['plcontinue']
             params['plcontinue'] = extra_data
 
+            #response
             response = session.get(url=url, params=params)
             data = response.json()
+
+            #get page data
             new_pages = data["query"]["pages"]
+
+            #items from the page
             for key, value in new_pages.items():
+
+                #links from the page
                 for link in value["links"]:
+
+                    #is regular link
                     if(link['ns'] == 0):
                         link_list.append(link["title"])
         except KeyError:
             break
         except json.decoder.JSONDecodeError:
             break
-        
-    #print(len(link_list))
     return link_list
 
-def seach_tree(start_node, end_value):
-
+#search the tree node links
+def seach_tree(start_node, end_value, query):
+    
+    #is start
     if start_node is None:
         return 
     
-    
+    #go through all of the links for that node
     for link in start_node.links:
 
-        if (link.node_value == end_value):
-            print('Found the end value')
-            
+        #has value and no other thread has found the path
+        if (link.node_value == end_value and not query.END ):
+            #print('Found the end value')
+            query.END = True
+
+            #return the link
             return link
 
+    #did not find the link --> return the original node
     return start_node
 
+#getting the right path
 def print_path(head):
+    
+    #path list
     path =  []
+
+    #temp for the list head
     temp = head
+
+    #go from the end point to start point and add to list
     while temp is not None:
-       # print(temp.node_value)
         path.append(temp.node_value)
         temp = temp.parent
     
+    #as the list is backwards reverse it
     path.reverse()
 
-    for index in range(len(path)):
-                    
-        if(index +1 < len(path)):
-            print(path[index], end=' --> ')
-        else:
-            print(path[index])
+    #for index in range(len(path)):
+    #                
+    #    if(index +1 < len(path)):
+    #        print(path[index], end=' --> ')
+    #    else:
+    #        print(path[index])
     return path
 
+#function that runs the algorithm. each thread run this function separitely
+def algo (root_node, end,q, query):
+    #https://onestepcode.com/graph-shortest-path-python/?utm_source=rss&utm_medium=rss&utm_campaign=graph-shortest-path-python
+    #loop as long as no path found
+    while not query.END:
 
-
-
-def worker (root_node, end):
-    global END
-    global Client_list
-    found = False
-    while not END:
-        
+        #path found
+        if (query.END):
+            break
+        # if queue has data
         if (not q.empty()):
+
+            #get next search node from queue
             root_node = q.get()
+
+            #get the search value
             search_term = root_node.node_value
+
+            #get links for that page
             links = wikipediaSearch(search_term)
+
+            #add links to tree for tha  root node
             for link in links:
                 root_node.add_link(link, root_node)
-            value_found = seach_tree(root_node, end)
+                
+                #add links to queue
+                q.put(root_node.links[-1])
+
+            #search the links for the end value
+            value_found = seach_tree(root_node, end, query)
+
+            #end value found
             if(value_found != root_node):
-                found = True
-            if(found):
+               
+                #add path to list
                 path = print_path(value_found)
 
-                q.task_done()
-                Client_list.append(path)
-                END = True
-                
-                
-            
+                #add query and set end to true so other threads know that path has been found
+                query.path = path
+                query.END = True
+                return 
 
-def search2 (start, end):
+#seach/worker unit that takes the query from the client and handles it           
+def search (start, end):
 
-    #links = proxy.wikipediaSearch(start)
-    #print(links)
-    print('new connection')
-    print(start)
+    #create a new query for this request
+    query = Query()
+    
+    #create new queue for this request
+    q = queue.Queue()
+    
+    #add start page to tree as root node and put it to queue
     root_node = TreeNode(start)
-  
     q.put(root_node)
+
+    #list for threads
     thread_list = []
-   
+    #st = time.time()
+
+    #create 10 threads that run algo() function that try to find the path
+    #https://www.tutorialspoint.com/python/python_multithreading.htm
     for i in range(10):
-        work =  threading.Thread(target=worker, args=(root_node, end ), daemon=True)
+        
+        work =  threading.Thread(target=algo, args=(root_node, end , q, query), daemon=True)
         work.start()
         thread_list.append(work)
+    
+    print('\nStarted 10 threads.')
+
+    #join the threads together
     for thread in thread_list:
         thread.join()
-    while True:
-        if(END):
-            return Client_list
-            break
 
-
-
-def Main2():
-    st = time.time()
-    search2("Halo 3", "London")
-    et = time.time()
-    took = et-st
-    print(took)
-    print(Client_list)
-    return Client_list
-#Main2()
-
-
-
-
-
-
-def iterateList(graph,end_topic,visited,topic,link):
-
-    #while True:
-        
-       # graph = item[0]
-       # end_topic = item[1]
-       #visited = item[2]
-       # topic = item[3]
-       # link = item[4]
-        #found = item[5]
-        #print(link)
-        value = []
-        if (link == end_topic):
-            print('Topic found')
-            
-            #print("It took",taken_time,"seconds")
-            graph[link] = graph[topic] + [link]
-            print(graph[link])
-            
-            #q.task_done() 
-            value = []
-            value.append(graph[link])
-            value.append(True)
-            
-            return   [graph[link], True]
-            
-        #if not
-        else:
-            #add new link to graph only if it is not there yet (could lead to interesting results otherwise)
-            if(link not in graph):
-
-                #add new link to graph. Idea {linkname: previous levels, linkname}
-                graph[link] = graph[topic] + [link]
-                #visited.append(link)
-                value.append(graph[link])
-                #value.append(visited)
-                value.append(False)
-                visited.append(link)
-                return  [link, False]
-            #return q.task_done() 
-        return [link, False]
-        
-
-       
-                
-      
-
-
-      
-
-def Main():
-    begin_time = time.time()
-    #start_topic = "Halo 3"
-    start_topic = "United States"
-    #start_topic = "Elizabeth II"
-    #end_topic = "Alligator"
-    #end_topic = "Bread"
-    #end_topic = "New York City Police Department"
-    #end_topic = "New York City"
-    end_topic = "London"
-    #end_topic = 'Elon Musk'
-    #https://www.geeksforgeeks.org/shortest-path-unweighted-graph/
-    graph = {}
-
- 
-    graph[start_topic] = [start_topic]
-    visited = []
-   
-    visited.append(start_topic)
-    index = 0
-    topic_list = wikipediaSearch(start_topic) #0...280
-    
+    #run as long as path is found
     while True:
 
-        #get links for the topic name
-        topic = visited[index]
-        topic_list = wikipediaSearch(topic)
-        results = []
-      
-        
-        #send_list = []
-        #https://stackoverflow.com/questions/26104512/how-to-obtain-the-results-from-a-pool-of-threads-in-python/26104609#26104609
-        pool = ThreadPool(30)
-        results = []
-        for link in topic_list:
-            results.append(pool.apply(iterateList, args=(graph,end_topic,visited,topic,link)))
-        pool.close()
-        pool.join()
-        
-        for r in results:
+        #path found and return the path to client
+        if(query.END):
+           # et = time.time()
+            #took = et-st
+            #print(took)
+            print('Threads closed')
+            return query.path
             
-
-            if(r[1] == True):
-        #        print(r[0])
-                end_time = time.time()
-                taken_time = end_time - begin_time
-                print("It took",taken_time,"seconds")
-                return 0
 
 
 #checking the correct seachterm
@@ -331,21 +299,7 @@ def checkCorrectParameters(parameter):
     else:
         return False
 
-#the algorithm function
-def search(start, end):
 
 
 
-    return ["Halo 3","7-Eleven","London"]
-server.register_function(checkCorrectParameters)
-server.register_function(search2)
-server.register_function(wikipediaSearch)
-#Main()
-
-def main():
-    try:
-        print('Starting server')
-        server.serve_forever() #runnin the server
-    except KeyboardInterrupt:
-        print('Shutting down the server')
-main()
+run_server()
